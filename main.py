1986bb252cffc8e8ddcbb52fa5727de24f1ccdce
Yash Raj Singh <40159239+yj1399@users.noreply.github.com>
2022-05-29 12:32:44 +0000

    Remove Output File Functianlity

--- main.py
+++ main.py
@@ -49,8 +49,6 @@ def process(command_params):
 
 
 if __name__ == "__main__":
-    # Opening Output File where all Output will be saved
-    sys.stdout = open('output.txt', 'wt')
     sys.stdin = open('input.txt' , "r")
     for line in sys.stdin:
       process(line)
